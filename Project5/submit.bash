8bb6a8d431a76ff4e31ebe91145cfea6441bf45a
Iain Moncrief <iainmoncrief@Iains-MacBook-Pro.local>
2023-05-25 16:39:04 -0700

    p5 rabbit push

--- Project5/submit.bash
+++ Project5/submit.bash
@@ -1,6 +1,16 @@
 #!/bin/bash
-
+#SBATCH -J MonteCarlo
+#SBATCH -A cs475-575
+#SBATCH -p classgpufinal
+#SBATCH --constraint=v100
+#SBATCH --gres=gpu:1
+#SBATCH -o montecarlo.out
+#SBATCH -e montecarlo.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=moncrief@oregonstate.edu
 
 # srun -A cs475-575 -p classgputest --pty bash run.bash
-srun -A cs475-575 -p classgpufinal --pty bash run.bash
+# srun -A cs475-575 -p classgpufinal --pty bash run.bash
+
+./run.bash
 
