7b41073fd0ea279d6f18f249a5664f8cc0171ae4
Iain Moncrief <iainmoncrief@Iains-MacBook-Pro.local>
2023-06-10 20:53:49 -0700

    Project 7

--- Project7/submit-test.bash
+++ Project7/submit-test.bash
@@ -2,8 +2,8 @@
 #SBATCH -J Project7
 #SBATCH -A cs475-575
 #SBATCH -p classmpifinal
-#SBATCH -N 16 # number of nodes
-#SBATCH -n 16 # number of tasks
+#SBATCH -N 8 # number of nodes
+#SBATCH -n 8 # number of tasks
 #SBATCH --constraint=ib
 #SBATCH -o proj07.out
 #SBATCH -e proj07.err
@@ -17,4 +17,3 @@ mpiexec -mca btl self,tcp -np 1 ./proj07
 mpiexec -mca btl self,tcp -np 2 ./proj07
 mpiexec -mca btl self,tcp -np 4 ./proj07
 mpiexec -mca btl self,tcp -np 8 ./proj07
-mpiexec -mca btl self,tcp -np 16 ./proj07
