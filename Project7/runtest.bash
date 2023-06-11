f877c6cec25166c8450ab246fb16c49ce7f40b14
Iain Moncrief <iainmoncrief@Iains-MacBook-Pro.local>
2023-06-10 20:15:58 -0700

    Project 7

--- Project7/runtest.bash
+++ Project7/runtest.bash
@@ -3,6 +3,6 @@
 # srun -A cs475-575 -p classmpitest --pty bash runtest.bash
 
 module load openmpi
-mpiexec -np 16 ./mpitest
+mpiexec -np 8 ./mpitest
 echo "--------------------- Trying Again ---------------------"
-mpiexec -mca btl self,tcp -np 16 ./mpitest
+mpiexec -mca btl self,tcp -np 8 ./mpitest
